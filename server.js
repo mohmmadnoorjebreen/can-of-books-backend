cf2276a617baf831b056f6ce73d7994cf45f0cb7
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-06-23 20:31:34 +0300

    lab14

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(express.json());
 const {getBook,creatBook,deleteBook,updateBook} = require('./controller/book.controller')
 require('dotenv').config();
 
+
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/book', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`${process.env.MONGO_URL}`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const seedUserData = require('./model/user.model')
 // seedUserData();
